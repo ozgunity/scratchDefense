3edb02e13f611cb032061836c79109cd79626bbe
Özgün ER <ozgunity@gmail.com>
2022-12-22 14:05:38 +0300

    Spawn Enemies Cart Done
    Yesterday I made the tilemap and today Instantiated a simple (temporary) enemy object. Tomorrow I will draw a lvl 1 enemy ane make it find and follow path.

---
+++ Scratch Defense/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector3 startPos;
+    
+    void Start()
+    {
+        
+        Instantiate(enemy, startPos,Quaternion.identity);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
+
+
+    //Move Towards Metodu
+//    [SerializeField]
+//    private Transform[] waypoints;
+
+    
+//    [SerializeField]
+//    private float moveSpeed = 2f;
+
+   
+//    private int waypointIndex = 0;
+
+    
+//    private void Start()
+//    {
+
+        
+//        transform.position = waypoints[waypointIndex].transform.position;
+//    }
+
+    
+//    private void Update()
+//    {
+
+        
+//        Move();
+//    }
+
+    
+//    private void Move()
+//    {
+//        if (waypointIndex <= waypoints.Length - 1)
+//        {
+
+            
+//            transform.position = Vector2.MoveTowards(transform.position,
+//               waypoints[waypointIndex].transform.position,
+//               moveSpeed * Time.deltaTime);
+
+            
+//            if (transform.position == waypoints[waypointIndex].transform.position)
+//            {
+//                waypointIndex += 1;
+//            }
+//        }
+//    }
+//}
